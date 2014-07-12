07cfac0d3d81def53c1ee85d7f9e99c24cb63f8c
Matt Patterson <matt.patterson94@gmail.com>
2014-07-12 15:59:52 +1000

    Style up map page

--- app/views/home/home.blade.php
+++ app/views/home/home.blade.php
@@ -3,19 +3,21 @@
 @section("content")
 
 <div class="container">
-	<section>
-		<div class="page-header">
-			<h1>Google Maps</h1>
-		</div>
-		<div class="row">
-			<div class="large-4 columns">
-				<h4>Toggle:</h4>
-				<ul class="no-bullet toggles"></ul>
-			</div>
-			<div class="large-8 columns">
-				<div id="map-canvas" class="flex-video"></div>
-			</div>
-		</div>
+	<section class="content">
+		<!-- <div class="row"> -->
+<!-- 			<div class="large-4 columns">
+				
+			</div> -->
+			<!-- <div class="large-8 columns"> -->
+		<div id="map-canvas" class="flex-video"></div>
+		
+		<section class="map-tools">
+			<h4>Toggle:</h4>
+			<ul class="no-bullet toggles"></ul>
+		</section>
+
+			<!-- </div> -->
+		<!-- </div> -->
 		<div class="row">
 			<div class="large-12 columns">
 				<div id="status"></div>
---
+++ app/views/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+	<div class="row">
+		<div class="medium-4 columns">
+			<img src="/img/logo.png">
+		</div>
+		<div class="medium-8 columns text-right">
+			<ul class="inline-list right">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Map</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+	<section class="page-header">
+		<div class="row">
+			<div class="medium-12 columns">
+				<h1>Google Maps</h1>
+			</div>
+		</div>
+	</section>	
+</header>
