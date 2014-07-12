d53a06732c1d3cc369f113152f41287b300908f3
Matt Patterson <matt.patterson94@gmail.com>
2014-07-12 16:45:54 +1000

    Possible sidebar - Map height is more than 100% :(

--- app/views/map/index.blade.php
+++ app/views/map/index.blade.php
@@ -8,18 +8,28 @@ Google Map
 
 <div class="container">
 	<section class="content">
-		<div id="map-canvas" class="flex-video"></div>
-		
-		<section class="map-tools">
-			<h4>Toggle:</h4>
-			<ul class="no-bullet toggles"></ul>
-		</section>
+		<div class="map">
+			<div id="map-canvas" class="flex-video"></div>
+			<section class="map-tools">
+				<h4>Toggle:</h4>
+				<ul class="no-bullet toggles"></ul>
+			</section>
+		</div>
 
 		<div class="row">
 			<div class="large-12 columns">
 				<div id="status"></div>
 			</div>
 		</div>
+
+		<section class="sidebar">
+			<ul class="sidenav">
+				<li><a href="#">Plan a journey</a></li>
+				<li><a href="#">Plan a journey</a></li>
+				<li><a href="#">Plan a journey</a></li>
+				<li><a href="#">Plan a journey</a></li>
+			</ul>
+		</section>	
 	</section>
 </div>
 
